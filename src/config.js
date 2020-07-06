e9a95b0346fafec33ab08d95bd0dd9a0e9520cc1
yugasun <yuga_sun@163.com>
2020-07-06 17:07:21 +0800

    fix: update default function description

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const CONFIGS = {
   timeout: 3,
   memorySize: 128,
   namespace: 'default',
-  description: 'This is a function created by serverless component'
+  description: 'Created by Serverless Component'
 }
 
 module.exports = CONFIGS
