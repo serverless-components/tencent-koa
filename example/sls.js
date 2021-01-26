1b729c48257c40dccb91fa5ed28905fc7a2bee4b
yugasun <yuga_sun@163.com>
2021-01-26 20:46:30 +0800

    test: fix

--- example/sls.js
+++ example/sls.js
@@ -1,5 +1,5 @@
 const Koa = require('koa')
-const KoaRouter = require('koa-router')
+const KoaRouter = require('@koa/router')
 const sendFile = require('koa-sendfile')
 const path = require('path')
 
@@ -9,7 +9,7 @@ const isServerless = process.env.SERVERLESS
 const PORT = 3000
 
 // Routes
-router.get(`/*`, async (ctx) => {
+router.get(`/`, async (ctx) => {
   await sendFile(ctx, path.join(__dirname, 'index.html'))
 })
 
